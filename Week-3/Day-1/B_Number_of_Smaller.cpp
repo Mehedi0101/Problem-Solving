175f7348ed4defbf9ec408f570caeecc741aca67
Mehedi0101 <mehedih2909@gmail.com>
2024-11-10 10:46:46 +0600

    solution of number of smaller is added

---
+++ Week-3/Day-1/B_Number_of_Smaller.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n,m;
+    cin >> n >> m;
+    map<ll,ll> mp;
+    for(ll i=1; i<=n; i++){
+        ll x;
+        cin >> x;
+        mp[x+1] = i;
+    }
+    // for(auto it=mp.begin(); it!=mp.end(); it++){
+    //     cout << it->first <<  " " << it->second << '\n';
+    // }
+    while(m--){
+        ll x;
+        cin >> x;
+        auto ub = mp.upper_bound(x);
+        if(ub == mp.begin()){
+            cout << 0 << " ";
+        }
+        else if(ub == mp.end()){
+            
+            cout << mp.rbegin()->second << " ";
+        }
+        else{
+            ub--;
+            cout << ub->second << " ";
+        }
+    }
+    return 0;
+}
