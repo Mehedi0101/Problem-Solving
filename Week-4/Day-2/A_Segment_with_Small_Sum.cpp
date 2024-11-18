7071bb1c7efeee0d2255aba4ce6dfcdd65912365
Mehedi0101 <mehedi29121998@gmail.com>
2024-11-18 18:02:27 +0600

    solution of segment with small sum is added

---
+++ Week-4/Day-2/A_Segment_with_Small_Sum.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n,s;
+    cin >> n >> s;
+    vector<ll> v(n);
+    for(ll i=0; i<n; i++){
+        cin >> v[i];
+    }
+    ll i=0, j=0, ans = 0, sum = 0;
+    while(j<n){
+        sum += v[j];
+        if(sum <= s){
+            ans = max(ans,j-i+1);
+        }
+        else{
+            sum -= v[i];
+            i++;
+        }
+        j++;
+    }
+    cout << ans << '\n';
+    return 0;
+}
