dd80946600e6b0abad000c3965023f7791bc49a7
Mehedi0101 <mehedih2909@gmail.com>
2024-11-27 10:40:51 +0600

    solution of dull operation is added

---
+++ Week-5/Day-4/Dull_Operation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while(t--){
+        ll x;
+        cin >> x;
+        cout << x << " " << x-1 << '\n';
+    }
+    return 0;
+}
