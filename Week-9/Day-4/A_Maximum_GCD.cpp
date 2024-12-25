7b9dbbe7ce45ceb9a03708484ae4091ae1ca4228
Mehedi0101 <mehedi29121998@gmail.com>
2024-12-25 10:21:59 +0600

    solution of maximum gcd is added

---
+++ Week-9/Day-4/A_Maximum_GCD.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define pl pair<long long,long long>
+#define Yes cout << "Yes" << '\n'
+#define yes cout << "YES" << '\n'
+#define No cout << "No" << '\n'
+#define no cout << "NO" << '\n'
+#define endl '\n'
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll T;
+    cin >> T;
+    while(T--){
+        ll n;
+        cin >> n;
+        cout << n/2 << endl;
+    }
+    return 0;
+}
