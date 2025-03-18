2c620daff27855f3008894acc88482ad5765ea50
Mehedi0101 <mehedi29121998@gmail.com>
2025-03-18 09:24:47 +0600

    solution of addition and minimum from codeforces is added

---
+++ Week-20/Day-2/A_Addition_and_Minimum.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pl pair<ll, ll>
+#define Yes cout << "Yes" << '\n'
+#define yes cout << "YES" << '\n'
+#define No cout << "No" << '\n'
+#define no cout << "NO" << '\n'
+#define endl '\n'
+using namespace std;
+
+const ll maxN = 1e5 + 9, inf = 1e18;
+ll a[maxN], t[maxN * 4], lazy[maxN * 4];
+
+void push(ll n, ll b, ll e)
+{
+    if (lazy[n] == 0)
+        return;
+    t[n] += lazy[n];
+    if (b != e)
+    {
+        ll l = (2 * n), r = (2 * n) + 1;
+        lazy[l] += lazy[n];
+        lazy[r] += lazy[n];
+    }
+    lazy[n] = 0;
+}
+
+void build(ll n, ll b, ll e)
+{
+    if (b == e)
+    {
+        t[n] = a[b];
+        return;
+    }
+    ll mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+    build(l, b, mid);
+    build(r, mid + 1, e);
+    t[n] = min(t[l], t[r]);
+}
+
+void update(ll n, ll b, ll e, ll i, ll j, ll v)
+{
+    push(n, b, e);
+    if (e < i || j < b)
+        return;
+    if (b >= i && e <= j)
+    {
+        lazy[n] = v;
+        push(n, b, e);
+        return;
+    }
+    ll mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+    update(l, b, mid, i, j, v);
+    update(r, mid + 1, e, i, j, v);
+    t[n] = min(t[l], t[r]);
+}
+
+ll query(ll n, ll b, ll e, ll i, ll j)
+{
+    push(n, b, e);
+    if (e < i || j < b)
+        return inf;
+    if (b >= i && e <= j)
+        return t[n];
+    ll mid = (b + e) / 2, l = (2 * n), r = (2 * n) + 1;
+    return min(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, m;
+    cin >> n >> m;
+    build(1, 1, n);
+    while (m--)
+    {
+        ll t;
+        cin >> t;
+        if (t == 1)
+        {
+            ll l, r, v;
+            cin >> l >> r >> v;
+            l++;
+            update(1, 1, n, l, r, v);
+        }
+        else
+        {
+            ll l, r;
+            cin >> l >> r;
+            l++;
+            cout << query(1, 1, n, l, r) << endl;
+        }
+    }
+    return 0;
+}
