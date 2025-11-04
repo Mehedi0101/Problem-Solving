b335801915d0691f57d4729512c5da8445a0b63b
Mehedi0101 <mehedih2909@gmail.com>
2025-11-04 22:00:44 +0600

    leetcode solution for 26 number problem is added

---
+++ Leetcode/26_remove_duplicates_from_the_sorted_array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> st;
+
+        for(int i=0; i<nums.size(); i++){
+            st.insert(nums[i]);
+        }
+
+        int j = 0;
+
+        for(int x:st){
+            nums[j] = x;
+            j++;
+        }
+
+        return st.size();
+    }
+};
