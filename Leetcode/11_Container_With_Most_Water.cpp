12ee9104acafcd221c71358be95f5fccb0d8988d
Mehedi Hasan <mehedih2909@gmail.com>
2026-03-08 15:09:42 +0600

    solution of container with most water (11) from leetcode is added

---
+++ Leetcode/11_Container_With_Most_Water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0, j=height.size()-1, ans=0;
+
+        while(i<j){
+            int x = min(height[i],height[j]);
+            ans = max(ans, x*(j-i));
+
+            if(height[i]>=height[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return ans;
+    }
+};
